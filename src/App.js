7d31ec09f69d3e04d20a12cc3aa4bbcb76b0fe60
Okan2513 <okanciicek1@gmail.com>
2026-01-20 17:17:26 +0100

    Change Header import to GlobalHeader component
    Updated the import for Header to use GlobalHeader instead.

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ import ProductDetailPage from "./pages/ProductDetailPage";
 import AdminPage from "./pages/AdminPage";
 
 // Components
-import  Header  from "./components/Header";
-import  Footer  from "./components/Footer";
+// BURAYI DEĞİŞTİRDİK: Artik "GlobalHeader" dosyasını okuyor
+import Header from "./components/GlobalHeader";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <LanguageProvider>
       <div className="App min-h-screen flex flex-col bg-slate-50 dark:bg-slate-950">
         <BrowserRouter>
+          {/* Buradaki Header artık GlobalHeader dosyanızdan geliyor */}
           <Header />
           <main className="flex-1">
             <Routes>
