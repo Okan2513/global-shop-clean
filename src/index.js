cbbddeb8ae354c60b92c44a99c2ce5f048147d79
Okan2513 <okanciicek1@gmail.com>
2026-01-21 22:24:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "./i18n";   // 🔥 i18n burada yüklenecek
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
