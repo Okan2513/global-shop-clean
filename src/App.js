3e02a13457a880786a4cbaa36c291e0709627e4c
Okan2513 <okanciicek1@gmail.com>
2026-01-21 19:26:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import "./App.css";
+import React from "react";
+// Vercel hatasını önlemek için alias (@) yerine göreceli yollar kullanıyoruz
+import "./App.css"; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
-// Pages
+// Sayfalar (Pages)
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import AdminPage from "./pages/AdminPage";
+// Yeni eklediğimiz yasal sayfalar
+import PrivacyPage from "./pages/PrivacyPage";
+import TermsPage from "./pages/TermsPage";
 
-// Components
+// Bileşenler (Components)
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
@@ -21,10 +26,17 @@ function App() {
           <Header />
           <main className="flex-1">
             <Routes>
+              {/* Ana Rotalar */}
               <Route path="/" element={<HomePage />} />
               <Route path="/products" element={<ProductsPage />} />
               <Route path="/products/:category" element={<ProductsPage />} />
               <Route path="/product/:id" element={<ProductDetailPage />} />
+              
+              {/* Yasal Rotalar (Yeni Eklenenler) */}
+              <Route path="/privacy" element={<PrivacyPage />} />
+              <Route path="/terms" element={<TermsPage />} />
+              
+              {/* Admin Paneli */}
               <Route path="/admin" element={<AdminPage />} />
               <Route path="/admin/*" element={<AdminPage />} />
             </Routes>
