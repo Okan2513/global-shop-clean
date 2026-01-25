2659d38cd77cee3bbc9d42c68cec8f54071b21d6
Okan2513 <okanciicek1@gmail.com>
2026-01-25 20:24:07 +0100

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -766,3 +766,7 @@ app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], all
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
